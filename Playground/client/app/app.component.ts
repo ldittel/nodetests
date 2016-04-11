9d74ec0d65b1028c9183551ec30e51dbc988ebd0
Nicholas Lawrence <nicholas@backdroplondon.com>
2016-04-11 21:04:25 +0100

    Major client update / test commit

--- Playground/client/app/app.component.ts
+++ Playground/client/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Sit in some vinegar to get that candy tight!</h1>'
+    template: '<h1>Sit in some vinegar to get that Ladi tight!</h1>'
 })
 export class AppComponent { }
